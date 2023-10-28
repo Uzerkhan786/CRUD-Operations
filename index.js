a3c87b67ba11da80b774431385af2b3e15314a1f
Uzerkhan786 <uzer.khan2020@vitbhopal.ac.in>
2023-10-28 19:37:39 +0530

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 const express=require('express');
 const PORT=3000;
 const app=express();
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
 
-app.get('/',(req,res)=>{
-   res.json({
-    title:"uzer",
-    des:"he is chutiya"
-   })
+let blogArray=[{name:"islam",age:24,id:2}];
+
+
+
+app.post('/blogs',(req,res)=>{
+    blogArray.push({
+        name:req.body.name,
+        age:req.body.age,
+        id:Math.floor(Math.random()*1000)
+    })
+    return res.json({
+data:blogArray
+    })
 })
+app.get('/blogs',(req,res)=>{
+    return res.json({
+        data:blogArray
+    })
+})
+
+// app.get('blogs/:id',(req,res)=>{
+    
+//     const ans=blogArray.filter((blog)=>blog.id==req.body.id)
+
+//      return res.json({
+//         data:ans
+//      })
+// })
+
+app.delete('/blogs/2',(req,res)=>{
+
+    console.log("ioejojf");
+})
+
+
 app.listen(PORT,()=>{
     console.log("server is started on "+PORT);
 })
